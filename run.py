b12b0de05aaa70a3dc31bfbe487ffb9457ccd379
Alexandros Stratoudakis <alexstrat4@gmail.com>
2024-08-12 20:17:36 +0300

    Update run.py
    Reformated runfile to be more user friendly.

--- run.py
+++ run.py
@@ -9,7 +9,17 @@ e-mail: alexstrat4@gmail.com
 from life_source import Life
 import utils
 
+# -------Parameters--------------
+N_grid = 30
+N_generations = 100
+init_seed = utils.Seeds.glider
+# -------Save-options------------
+save = False
+save_name = 'animations\\glider'   # do not add file extension
+fps = 24
+# -------------------------------
+
 if __name__ == '__main__':
-    L = Life(n_grid=20, seed=utils.glider)
-    L.n_generations(100)
-    anim = L.animate(save=False, name='animations\\glider')
+    L = Life(n_grid=N_grid, seed=init_seed)
+    L.n_generations(n=N_generations)
+    anim = L.animate(save=save, name=save_name, fps=fps)
